4765d22571027aad5bf683a7d87ed849bee51b03
Rodrigo <rodrigo@Rodrigos-MacBook-Pro-2.local>
2024-02-23 10:39:05 -0600

    quinto commit

--- src/components/TwoColumnInverted/TwoColumnInverted.js
+++ src/components/TwoColumnInverted/TwoColumnInverted.js
@@ -6,6 +6,7 @@ import texts from "../../text/global.json";
 function TwoColumnInverted({ page, section }) {
   const body = texts[page][section].body;
   const title = texts[page][section].title;
+  const subheader = texts[page][section].subheader;
   const url = texts[page][section].url;
 
   return (
@@ -13,8 +14,9 @@ function TwoColumnInverted({ page, section }) {
       <Container className="py-md-5 pb-5">
         <Row>
           <Col md={6}>
-            <h1 className="mb-3">{title}</h1>
-            <p className="mt-4">{body}</p>
+            <h5 className="text-muted">{subheader}</h5>
+            <h1 className="mb-5">{title}</h1>
+            <p>{body}</p>
           </Col>
           <Col className="logo-img" md={6}>
             <img className="sticky-top" src={url} />
